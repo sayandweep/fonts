812f4fdb6f55d6eb6573142a6671bc4974b4529c
sayandweep <81100142+sayandweep@users.noreply.github.com>
2021-05-21 12:52:18 +0530

    Update unikizwame.js

--- unikizwame.js
+++ unikizwame.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 // Check if href is changed
-if ($("#mycontent").attr("href") != "https://wa.me/+917318839342?text=Hello%20Unikiz") {
+if ($("#mycontent").attr("href") != "https://wa.me/+917318839342?text=Hello") {
  window.location.href = "https://wa.me/+917318839342?text=Hello%20Unikiz"; // Redirect destination link
 };
     
