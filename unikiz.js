a9161a2cc74f49f18aab44a02fc11272ecc7849c
sayandweep <81100142+sayandweep@users.noreply.github.com>
2021-05-21 09:57:58 +0530

    Update unikiz.js

--- unikiz.js
+++ unikiz.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 // Check if href is changed
-if ($("#mycontent").attr("href") != "https://youtube.com/channel/UC8WTpzPSZdUnSKn37-5WbVw") {
+if ($("#mycontent").attr("href") != "https://wa.me/+917318839342?text=Hello%20Unikiz") {
  window.location.href = "https://wa.me/+917318839342?text=Hello%20Unikiz"; // Redirect destination link
 };
     
