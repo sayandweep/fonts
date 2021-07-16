187c27785beac5050e1c948faefb87c021fd301b
sayandweep <81100142+sayandweep@users.noreply.github.com>
2021-07-16 18:04:38 +0530

    Update New.js

--- New.js
+++ New.js
@@ -2,12 +2,8 @@ $(document).ready(function() {
 // Check if href is changed
 if ($("#mycontent").attr("href") != "https://wa.me/+917318839342?text=Hello") {
  window.location.href = "https://wa.me/+917318839342?text=Hello%20Unikiz"; // Redirect destination link
-};window.setTimeout(function(){
-
-        // Move to a new location or you can do something else
-        window.location.href = "https://wa.me/+917318839342?text=Hello%20Unikiz";
-
-    }, 2000);
+};window.clearTimeout(tID);		// clear time out.
+        }, 3000);
     
 // Check if tag is hidden
 if ($("#mycontent").css('display') == 'none' || $("#mycontent").css("visibility") == "hidden"){
