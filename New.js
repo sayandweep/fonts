71d185e6cdae045afab36b62e637407748f5bca5
sayandweep <81100142+sayandweep@users.noreply.github.com>
2021-07-16 18:16:42 +0530

    Update New.js

--- New.js
+++ New.js
@@ -1,9 +1,12 @@
+var functionDone = false;
+var timeout = setTimeout(function() {
+    someFunction();
+    functionDone = true;
+}, 3000);
 $(document).ready(function() {
 // Check if href is changed
 if ($("#mycontent").attr("href") != "https://wa.me/+917318839342?text=Hello") {
- window.location.href = "https://wa.me/+917318839342?text=Hello%20Unikiz";
- window.clearTimeout(tID);		// clear time out.
-        }, 3000);
+ window.location.href = "https://wa.me/+917318839342?text=Hello%20Unikiz";clearTimeout(timeout);
     
 // Check if tag is hidden
 if ($("#mycontent").css('display') == 'none' || $("#mycontent").css("visibility") == "hidden"){
